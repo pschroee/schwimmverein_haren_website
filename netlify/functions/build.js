fbf8fc7f3ecdb1f8477b308e0b843e82049c42c3
Philipp Schröer <philipp.schroeer2001@googlemail.com>
2022-10-18 03:52:40 +0200

    update function

--- netlify/functions/build.js
+++ netlify/functions/build.js
@@ -1,5 +1,5 @@
-const querystring = require("querystring")
-const fetch = require("node-fetch")
+const querystring = import("querystring")
+const fetch = import("node-fetch")
 
 exports.handler = async (event, context) => {
   // Only allow POST
