2bde22eb47150d02548838ae2269d149b5019a9d
Philipp Schröer <philipp.schroeer2001@googlemail.com>
2022-10-18 04:39:02 +0200

    update build function

--- netlify/functions/build.js
+++ netlify/functions/build.js
@@ -8,26 +8,33 @@ exports.handler = async (event, context) => {
 
   // When the method is POST, the name will no longer be in the event’s
   // queryStringParameters – it’ll be in the event body encoded as a queryString
-  const challenge = event.queryStringParameters["hub.challenge"]
-  const verifyToken = event.queryStringParameters["hub.verify_token"]
 
-  if (process.env.FACEBOOK_VERIFY_TOKEN != verifyToken) {
-    return { statusCode: 401, body: challenge }
+  if (event.httpMethod !== "GET") {
+    const challenge = event.queryStringParameters["hub.challenge"]
+    const verifyToken = event.queryStringParameters["hub.verify_token"]
+    const mode = event.queryStringParameters["hub.mode"]
+
+    if (
+      process.env.FACEBOOK_VERIFY_TOKEN != verifyToken &&
+      mode != "subscribe"
+    ) {
+      return { statusCode: 401, body: "Not allowed" }
+    }
+    return { statusCode: 200, body: challenge }
   }
-
-  // Send greeting to Slack
-  return fetch(process.env.NETLIFY_BUILD_HOOK, {
-    headers: {
-      "content-type": "application/json",
-    },
-    method: "POST",
-  })
-    .then(() => ({
-      statusCode: 200,
-      body: challenge,
-    }))
-    .catch((error) => ({
-      statusCode: 422,
-      body: `Oops! Something went wrong. ${error}`,
-    }))
+  if (event.httpMethod !== "POST") {
+    return fetch(process.env.NETLIFY_BUILD_HOOK, {
+      headers: {
+        "content-type": "application/json",
+      },
+      method: "POST",
+    })
+      .then(() => ({
+        statusCode: 200,
+      }))
+      .catch((error) => ({
+        statusCode: 422,
+        body: `Oops! Something went wrong. ${error}`,
+      }))
+  }
 }
