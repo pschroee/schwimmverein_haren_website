b8fe4d51c331b30267dd81beb16fa60db785d89a
Philipp Schröer <philipp.schroeer2001@googlemail.com>
2022-12-07 20:30:05 +0100

    add astro image component for external images from facebook

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-
-import compress from "astro-compress";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import sitemap from "@astrojs/sitemap"
+import compress from "astro-compress"
+import image from "@astrojs/image"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), compress()]
-});
+  integrations: [
+    tailwind(),
+    sitemap(),
+    compress(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
+})
