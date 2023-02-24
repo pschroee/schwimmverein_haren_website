47613987fcdcde1c7c3f0e0f7d77c911ea3da314
Philipp Schröer <philipp.schroeer2001@googlemail.com>
2023-02-24 18:19:45 +0100

    Facebook News: Filter shared content

--- src/lib/facebook.js
+++ src/lib/facebook.js
@@ -12,11 +12,18 @@ export async function getPosts() {
           "place",
           "permalink_url",
           "attachments{media,title}",
+          "status_type",
         ].join(","),
         access_token: import.meta.env.FACEBOOK_ACCESS_TOKEN,
         limit: 10,
       })
   ).then((response) => response.json())
-  const filteredPostsWithMessages = data.data.filter((post) => post.message)
+  const filteredPostsWithMessages = data.data.filter((post) => {
+    if (post.message && post.status_type !== "shared_story") {
+      return true
+    } else {
+      return false
+    }
+  })
   return filteredPostsWithMessages.slice(0, 3)
 }
